4d5ac0b6c668ee174f2b9cb37c55ebcd4f93cbd4
murali manohar <manohar.karpi@gmail.com>
2025-02-09 18:35:51 +0530

    first commit

---
+++ reviewsystem/reviewsystem/src/test/java/com/xyz/reviewsystem/ReviewsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.reviewsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
